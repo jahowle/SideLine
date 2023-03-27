a87081a626a765f57158eb9ed2b944623a2e45b4
Jonny <jonny.howle@gmail.com>
2023-03-27 17:13:34 -0600

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          SideLine Coming Soon
         </a>
       </header>
     </div>
