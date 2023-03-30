771a6bd35ccb67e19c6f60218103e95fcf7f9352
Jonny <jonny.howle@gmail.com>
2023-03-29 21:16:09 -0600

    modified packages

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,29 @@
+class Api::UsersController < ApplicationController
+
+    wrap_parameters format: []
+
+    skip_before_action :authorize, only: [:create]
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+          render json: user, status: :created
+        else
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        user = User.find_by_id(session[:user_id])
+        render json: user
+    end
+    
+    private
+
+    # Need to figure out why using is causing an error
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :wins, :losses, :balance)
+    end
+
+end
