3c7479e34ba4b81cd5601e45e91ee3535ad67def
Jonny <jonny.howle@gmail.com>
2023-03-29 21:14:41 -0600

    reinstalled packages

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import { UserContext } from "../context/user";
 
 function Login() {
+    const history = useHistory();
 
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
