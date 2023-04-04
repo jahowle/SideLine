dc574fbbe030b6106359b1c28f0991af6adc6129
Jonny <jonny.howle@gmail.com>
2023-04-04 12:25:13 -0600

    added info columns to users for OAuth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_23_161924) do
+ActiveRecord::Schema.define(version: 2023_04_04_182310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2023_03_23_161924) do
     t.integer "balance"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
+    t.string "provider"
   end
 
 end
