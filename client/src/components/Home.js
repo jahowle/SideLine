fc6abd86e4b6e6ab77b7edd85901b3048d978b55
Jonny <jonny.howle@gmail.com>
2023-04-09 11:20:41 -0600

    upgraded react, react-dom, react-scripts and installed tailwind

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -11,6 +11,9 @@ function Home({wagers}) {
 
     return (
         <div>
+             <h1 className="text-4xl font-bold underline text-amber-600">
+      Hello world!
+    </h1>
             <h2>This is home</h2>
             {wagerCards}
         </div>
